014c9750eb3149449c5776ccd62c93108cec7d9f
julia340 <zieba111j@gmail.com>
2024-03-17 17:42:52 +0100

    dokonczenie podpunktu 2 z zad

--- lab3/lab3.cpp
+++ lab3/lab3.cpp
@@ -30,9 +30,43 @@ void wypisz_macierz(int macierz[rozmiar][rozmiar]){
     }
 }
 
+int suma_diagonali(int macierz[rozmiar][rozmiar]){
+    int suma = 0;
+    for(int i = 0; i < rozmiar; i++){
+        suma += macierz[i][i];
+    }
+    return suma;
+}
+
+void mnozenie_przez_skalar(int macierz[rozmiar][rozmiar], int skalar){
+    for(int i = 0; i < rozmiar; i++){
+        for(int j = 0; j < rozmiar; j++){
+            macierz[i][j] *= skalar;
+        }
+    }
+
+}
+void mnozenie_przez_siebie(int macierz[rozmiar][rozmiar]){
+    for(int i = 0; i < rozmiar; i++){
+        for(int j = 0; j < rozmiar; j++){
+            macierz[i][j] *= macierz[i][j];
+        }
+    }
+
+}
+
 int main(){
+
     int macierz[rozmiar][rozmiar];
+   
     wczytaj_macierz("./data.txt", macierz);
     wypisz_macierz(macierz);
+    
+    int suma = suma_diagonali(macierz);
+    cout << "Suma diagonali wynosi : " << suma << endl;
+    
+    mnozenie_przez_siebie(macierz);
+    wypisz_macierz(macierz);
+    
     return 0;
 }
