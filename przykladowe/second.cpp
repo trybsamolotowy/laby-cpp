661d602ab7d62cda562b68fc4ca0e88968849d10
ZephyrZenith <zieba111j@gmail.com>
2024-06-23 12:39:20 +0200

    update

--- przykladowe/second.cpp
+++ przykladowe/second.cpp
@@ -1,19 +1,45 @@
 #include <iostream>
-
 using namespace std;
 
-
-class hello{
-
+class hello {
 private:
-
-    string msg;
+    string msg;  // Prywatne pole msg
 
 public:
+    hello(string a) : msg(a) {}  // Publiczny konstruktor z parametrem do inicjalizacji pola msg
+    hello() : msg("Hello") {}  // Domyślny konstruktor
 
-    hello(string a):msg(a){}
-
-    void virtual toString(){
+    virtual string toString() const {  // Publiczna wirtualna metoda toString(), zwracająca pole msg obiektu
         return msg;
     }
+
+    void display() {
+        cout << msg << endl;
+    }
 };
+
+class world : public hello {  // Dziedziczenie publiczne, umożliwiające dostęp do składowych klasy bazowej
+private:
+    string msg2;  // Prywatne pole msg2
+
+public:
+    world(string a, string b) : hello(a), msg2(b) {  // Publiczny konstruktor z parametrami do inicjalizacji pól msg i msg2
+        // Inicjalizacja pola msg klasy bazowej za pomocą hello(a)
+        // Inicjalizacja pola msg2 za pomocą msg2(b)
+    }
+
+    string toString() const override {  // Publiczna metoda toString(), zwracająca łańcuch złożony z wartości pól msg i msg2, oddzielonych spacją
+        return hello::toString() + " " + msg2;
+    }
+};
+
+int main() {
+    hello h1;  // Błąd: 'hello h1();' deklaruje funkcję zamiast tworzyć obiekt. Poprawiono na 'hello h1;'
+    hello h2("Hello World");
+    h2.display();
+
+    world w1("Hello", "World");  // Poprawiono konstruktor klasy world do inicjalizacji dwóch pól
+    cout << w1.toString() << endl;  // Wyświetlenie połączonego msg z klasy bazowej i pochodnej
+
+    return 0;
+}
